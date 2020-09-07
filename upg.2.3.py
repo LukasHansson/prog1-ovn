6ec59da5aee291ea45da4b329f451f78dcfbba78
Lukas Hansson <lukas.hansson@elev.ga.ntig.se>
2020-09-07 14:37:03 +0200

    2.3

---
+++ upg.2.3.py
@@ -0,0 +1,10 @@
+svar = input ('Skriv ett tal: ')
+x = float (svar)
+y = x * x
+i = 19
+print(f'Resultat:{i}')
+j = 107
+print(f'Resultat:{j:5}')
+k = 4
+print(f'Resultat:{k:03}')
+print (f'Talet i kvadrat är{y:.2f}')
